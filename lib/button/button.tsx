08b9f8f9fc84adfdaadd9d53bc2113a418b5c38e
Liam.Li <liam.li@avepoint.com>
2020-06-05 16:54:58 +0800

    add prefixCls-provider

--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import { tuple } from '../_util/type';
+import { ConfigConsumer } from '../config-provider/index';
 
 const ButtonTypes = tuple('default', 'primary', 'ghost', 'dashed', 'danger', 'link');
 export type ButtonType = typeof ButtonTypes[number];
@@ -12,7 +13,13 @@ export interface BaseButtonProps {
 
 const Button = (): JSX.Element => {
     return (
-        <div>Button2</div>
+        <ConfigConsumer>
+            {
+                (context) => {
+                    return <div>{context.getPrefixCls("btn")}</div>
+                }
+            }
+        </ConfigConsumer>
     )
 }
 
